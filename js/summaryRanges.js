82bb4014fe2dfa71bb3c7a5691c7f7c57b7dfc6b
xuyang <xuyang@xuyangdeMacBook-Pro.local>
2015-06-27 22:27:07 +0800

    add  description of problems

--- js/summaryRanges.js
+++ js/summaryRanges.js
@@ -1,5 +1,10 @@
 /**
- * leetcode No.228 Summary Ranges
+ *
+ No.228 Summary Ranges
+    Given a sorted integer array without duplicates, return the summary of its ranges.
+
+    For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
+
  * @param {number[]} nums
  * @return {string[]}
  *
