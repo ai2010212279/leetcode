f59e5a6d9b6595684d4430723cae7bafdaea895c
xuyang <ai2010212279@gmail.com>
2015-08-26 02:05:24 +0800

    完成Letter Combinations Of A Phone Number

---
+++ js/letterCombinationsOfAPhoneNumber.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+
+
+
+var letterCombinations = function(digits) {
+    var numberDic = [];
+    numberDic[0] = [];
+    numberDic[1] = [];
+    numberDic[2] = ['a','b','c'];
+    numberDic[3] = ['d','e','f'];
+    numberDic[4] = ['g','h','i'];
+    numberDic[5] = ['j','k','l'];
+    numberDic[6] = ['m','n','o'];
+    numberDic[7] = ['p','q','r','s'];
+    numberDic[8] = ['t','u','v'];
+    numberDic[9] = ['w','x','y','z'];
+
+    i = 0;
+    temp = [];
+    while(i < digits.length){
+        number = parseInt(digits[i]);
+        if(number === 0 || number === 1) {
+            i++;
+            continue;
+        }
+
+        if(temp.length === 0){
+            temp = numberDic[number];
+        }else{
+            temp1 = [];
+            for(j = 0;j < temp.length;j++){
+                for(k = 0; k < numberDic[number].length;k++){
+                    temp1.push(temp[j] + numberDic[number][k]);
+                }
+            }
+            temp = temp1;
+        }
+        i++;
+    }
+    return temp;
+};
+
+
+a = "1204";
+b = letterCombinations(a);
+
+
+console.log(b);
+
