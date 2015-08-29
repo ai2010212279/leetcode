7aec1db548a543d2275a93e41c76068d9c419e39
xuyang <ai2010212279@gmail.com>
2015-08-29 23:21:58 +0800

    finish Longest Valid Parentheses

--- js/longestValidParentheses.js
+++ js/longestValidParentheses.js
@@ -10,42 +10,143 @@ Another example is ")()())", where the longest valid parentheses substring is "(
  * @return {number}
  目前是错误的
  */
+// var longestValidParentheses = function(s) {
+//     var leftDeep = 0,rightDeep = 0;
+//     var longestParentheses = 0;
+//     var temp2 = 0,temp1 = 0;
+//     for(i = 0;i < s.length;i++){
+//         if(s[i] == '('){
+//             leftDeep++;
+//         }
+//         if(s[i] == ')'){
+//             rightDeep++;
+//             if(leftDeep >= rightDeep){
+//                 leftDeep--;
+//                 rightDeep--;
+//                 temp2 += 2;
+//                 if(leftDeep==0){
+//                     temp1 += temp2;
+//                     temp2 = 0;
+//                 }
+//             }else{
+//                 rightDeep--;
+//                 longestParentheses = longestParentheses > temp1 ? longestParentheses : temp1;
+//                 temp1 = 0;
+//             }
+//         }
+//     }
+//     longestParentheses = longestParentheses > temp1 ? longestParentheses : temp1;
+//    console.log(leftDeep,temp1,temp2);
+//     longestParentheses = longestParentheses > temp2 ? longestParentheses : temp2;
+
+//     return longestParentheses;
+// };
+
+// var longestValidParentheses = function(s) {
+//     var i = 0;
+//     var leftDeep = 0;
+//     while(i < s.length){
+//         if(s[i] == '('){
+//             if(s[i+1] == '('){
+//                 leftDeep++;
+//             }else if(s[i+1] == ')'){
+//                 leftDeep--;
+//             }
+//         }
+//     }
+
+// }
+
 var longestValidParentheses = function(s) {
-    var maxLength = 0;
-    var tempLength = 0;
-    var calculate = 0;
-    for(i = 0,j = 0;i < s.length;){
-
+    var i = 0,j = 0;
+    var tempArr = [];
+    while(i < s.length){
+        if(s[i] === '('){
+            tempArr.push(i);
+            j = tempArr.length - 1;
+        }else{
+            if(s[tempArr[j]] === '('){
+                tempArr[j] = -1;
+                tempArr.push(-1);
+            }else if(tempArr[j] === -1){
+                while(tempArr[j] === -1){
+                    j--;
+                }
+                if(s[tempArr[j]] === '('){
+                    tempArr[j] = -1;
+                    tempArr.push(-1);
+                }else{
+                    tempArr.push(i);
+                }
+            }else{
+                tempArr.push(i);
+            }
+        }
+        i++;
     }
-};
+    i = 0;
+    var maxLength = 0;
+    tempLength  = 0;
 
-a = '(((())';
+    while(i < tempArr.length){
+        if(tempArr[i] === -1){
+            tempLength++;
+        }else{
+            maxLength = maxLength > tempLength ? maxLength : tempLength;
+            tempLength = 0;
+        }
+        i++;
+    }
+
+    maxLength = maxLength > tempLength ? maxLength : tempLength;
+
+    return maxLength;
+}
+
+a = "(()(((()"; //2
 b = longestValidParentheses(a);
-console.log(b);
+// console.log(b);
 
-a = '()()()(((())))';
+a = '(((())';//4
 b = longestValidParentheses(a);
-console.log(b);
+// console.log(b);
 
-a = '(((((())))))';
+a = '()()()(((())))';//14
 b = longestValidParentheses(a);
-console.log(b);
+// console.log(b);
 
-a = '))))';
+a = '(((((())))))';//12
 b = longestValidParentheses(a);
-console.log(b);
+// console.log(b);
 
-a = ')())';
+a = '))))';//0
 b = longestValidParentheses(a);
-console.log(b);
+// console.log(b);
 
-a = '(()';
+a = ')())';//2
 b = longestValidParentheses(a);
-console.log(b);
+// console.log(b);
 
-
-a = '()()((';
+a = '(()';//2
 b = longestValidParentheses(a);
-console.log(b);
+// console.log(b);
+
+
+a = '()()((';//4
+b = longestValidParentheses(a);
+// console.log(b);
+
+
+a = "()(((((((())";//4
+b = longestValidParentheses(a);
+// console.log(b);
+
+a = "()())))()()())))()()";//6
+b = longestValidParentheses(a);
+// console.log(b);
+
+a = '(()((()))())';//12
+b = longestValidParentheses(a);
+// console.log(b);
 
 
