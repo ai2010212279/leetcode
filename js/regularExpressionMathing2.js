64cd6a9f9b79327c94e218991b589b2560df2caa
xuyang <ai2010212279@gmail.com>
2015-08-30 17:34:26 +0800

    例子通过验证，准备提交

--- js/regularExpressionMathing2.js
+++ js/regularExpressionMathing2.js
@@ -17,17 +17,24 @@ var isMatch = function(s, p) {
             result = false;
             // console.log('a',i,j);
             while(i < s.length && j+1 < p.length){
-                console.log(i,j);
+                // console.log(i,j);
                 result = compare(s,i,p,j+2);
                 if(result){
                     return result;
                 }
                 ++i;
             }
-            return compare(s,i,p,j);
-        }else if(p[j] === '.'){
+            // console.log('*',i,j);
+            if(i === s.length){
+                return compare(s,i,p,j+2);
+            }else{
+                return false;
+            }
+        }else if(p[j] === '.' && i < s.length){
+            // console.log('.',i,j);
             return compare(s,i+1,p,j+1);
-        }else if(p[j] === s[i]){
+        }else if(p[j] === s[i] && i < s.length){
+            // console.log('=',i,j);
             return compare(s,i+1,p,j+1);
         }else{
             return false;
@@ -38,37 +45,38 @@ var isMatch = function(s, p) {
 
 
 
-// a = isMatch("aa","a");//false
-// console.log(a);
-// a = isMatch("aaa","aa");//false
-// console.log(a);
-// a = isMatch('aa','*');//false
-// console.log(a);
-// a = isMatch('ab','.*c');//false
-// console.log(a);
-// a = isMatch('a','ab*c***');//false
-// console.log(a);
-// a = isMatch("aa", "a*");//true
-// console.log(a);
-// a = isMatch("aa", ".*");//true
-// console.log(a);
-// a = isMatch("aa","aa");//true
-// console.log(a);
-// a = isMatch("ab", ".*");//true
-// console.log(a);
-// a = isMatch("aab", "c*a*b");//true
-// // a = isMatch("ab",".*c");
-// console.log(a);
-// a = isMatch('a','a*');//true
-// console.log(a);
-// a = isMatch('aaa','a*aaa');//true
-// console.log(a);
-// a = isMatch('abcdef','.*def');//true
-// console.log(a);
-// a = isMatch("aaa", "ab*a*c*a");//true
-// console.log(a);
-// a = isMatch('aaaa','a*aaaaa*');//true
-// console.log(a);
-a = isMatch('a','.*..');
+a = isMatch("aa","a");//false
 console.log(a);
+a = isMatch("aaa","aa");//false
+console.log(a);
+a = isMatch('aa','*');//false
+console.log(a);
+a = isMatch('ab','.*c');//false
+console.log(a);
+a = isMatch('a','ab*c***');//false
+console.log(a);
+a = isMatch('a','.*..');//false
+console.log(a);
+a = isMatch("aa", "a*");//true
+console.log(a);
+a = isMatch("aa", ".*");//true
+console.log(a);
+a = isMatch("aa","aa");//true
+console.log(a);
+a = isMatch("ab", ".*");//true
+console.log(a);
+a = isMatch("aab", "c*a*b");//true
+// a = isMatch("ab",".*c");
+console.log(a);
+a = isMatch('a','a*');//true
+console.log(a);
+a = isMatch('aaa','a*aaa');//true
+console.log(a);
+a = isMatch('abcdef','.*def');//true
+console.log(a);
+a = isMatch("aaa", "ab*a*c*a");//true
+console.log(a);
+a = isMatch('aaaa','a*aaaaa*');//true
+console.log(a);
+
 
