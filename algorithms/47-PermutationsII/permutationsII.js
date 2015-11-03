8ff60901a4c9ee5191bf635109754c7fcf4ce451
xuyang <ai2010212279@gmail.com>
2015-11-03 10:51:10 +0800

    use next permutation finish permutationII

--- algorithms/47-PermutationsII/permutationsII.js
+++ algorithms/47-PermutationsII/permutationsII.js
@@ -6,27 +6,60 @@
 
 var permuteUnique = function (nums) {
     result = [];
+    nums.sort(function(a,b){
+        return a > b ? 1 : -1;
+    });
     result.push(nums.slice());
-    var i = nums.length - 2;
-    while(i > -1){
-        if(nums[i] < nums[i + 1]){
-            temp = nums[i];
-            nums[i] = nums[i + 1];
-            nums[i + 1] = temp;
-
-            for (var j = nums[nums.length - 1], k = i + 1; j > k;k++,j--){
-                temp = nums[k];
-                nums[k] = nums[j];
-                nums[j] = temp;
-            }
-            nums1 = nums.slice();
-            result.push(nums1);
-            i = nums.length - 1;
-        }
-        i--;
+    while(nextPermutation(nums)){
+        result.push(nums.slice());
     }
+
     return result;
 }
+
+var nextPermutation = function(nums) {
+    var flag = 1;
+    var descMaxIndex = nums.length - 1;
+    while(flag && descMaxIndex > 0){
+        if(nums[descMaxIndex] <= nums[descMaxIndex - 1]){
+            descMaxIndex--;
+        }else{
+            flag = 0;
+        }
+    }
+
+
+    var exchangeIndex = nums.length - 1;
+    if(descMaxIndex-1 >= 0){
+        while(exchangeIndex > descMaxIndex-1){
+            if(nums[exchangeIndex] > nums[descMaxIndex-1]){
+                temp = nums[descMaxIndex-1];
+                nums[descMaxIndex-1] = nums[exchangeIndex];
+                nums[exchangeIndex] = temp;
+                reverseArr(nums,descMaxIndex,nums.length - 1);
+                break;
+            }else{
+                exchangeIndex--;
+            }
+        }
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+function reverseArr(arr,left,right){
+    while(left < right){
+        temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+}
+
+a = permuteUnique([1,2,3]);
+console.log(a);
 
 a = permuteUnique([1,1,2]);
 console.log(a);
